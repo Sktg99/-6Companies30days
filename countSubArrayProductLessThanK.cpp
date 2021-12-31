aa5a7a1c3e58d29e2d626e049ceb55814821b312
Sanchit Kumawat <56692432+Sktg99@users.noreply.github.com>
2021-12-31 15:49:35 +0530

    Create countSubArrayProductLessThanK.cpp

---
+++ countSubArrayProductLessThanK.cpp
@@ -0,0 +1,16 @@
+class Solution{
+  public:
+    int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
+       long long  product=1,j=0,ans=0;
+       for(int i=0;i<n;i++)
+       {
+           product*=a[i];
+           while(j<n && product >= k)
+           {
+               product/=a[j++];
+           }
+           ans+=(i-j+1);
+       }
+       return ans;
+    }
+};
